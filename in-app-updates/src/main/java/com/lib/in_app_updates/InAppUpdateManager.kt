170d9158af029e48e5f26075d7670d2ac9ec02ad
Ruslan Kahalniak <r.kahalniak@lampalampa.net>
2022-10-27 13:49:40 +0300

    Initial commit

---
+++ in-app-updates/src/main/java/com/lib/in_app_updates/InAppUpdateManager.kt
@@ -0,0 +1,89 @@
+package com.lib.in_app_updates
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType.FLEXIBLE
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
+
+class InAppUpdateManager(activity: Activity,private val view: View) : InstallStateUpdatedListener {
+
+    private var appUpdateManager: AppUpdateManager
+    private val UPDATE_REQUEST_CODE = 500
+    private var parentActivity: Activity = activity
+
+    private var currentType = FLEXIBLE
+
+    init {
+        appUpdateManager = AppUpdateManagerFactory.create(parentActivity)
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+            if (info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                startUpdate(info, FLEXIBLE)
+            } else {
+                // UPDATE IS NOT AVAILABLE
+            }
+        }
+        appUpdateManager.registerListener(this)
+    }
+
+
+    private fun startUpdate(info: AppUpdateInfo, type: Int) {
+        appUpdateManager.startUpdateFlowForResult(info, type, parentActivity, UPDATE_REQUEST_CODE)
+        currentType = type
+    }
+
+    fun onResume() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+            if (currentType == FLEXIBLE) {
+                if (info.installStatus() == InstallStatus.DOWNLOADED)
+                    flexibleUpdateDownloadCompleted()
+            } else if (currentType == IMMEDIATE) {
+                if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                    startUpdate(info, IMMEDIATE)
+                }
+            }
+        }
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == UPDATE_REQUEST_CODE) {
+            if (resultCode != AppCompatActivity.RESULT_OK) {
+                // If the update is cancelled or fails, you can request to start the update again.
+                Log.e("ERROR", "Update flow failed! Result code: $resultCode")
+            }
+        }
+    }
+
+    private fun flexibleUpdateDownloadCompleted() {
+        Snackbar.make(
+            view,
+            "An update has just been downloaded.",
+            Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            setActionTextColor(Color.WHITE)
+            show()
+        }
+    }
+
+    fun onDestroy() {
+        appUpdateManager.unregisterListener(this)
+    }
+
+    override fun onStateUpdate(state: InstallState) {
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            flexibleUpdateDownloadCompleted()
+        }
+    }
+}
